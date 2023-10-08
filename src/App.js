14b323e36d02a9e82f03e866acd92a3003c59730
ThanawaratGithub <lopamarkin@gmail.com>
2023-10-09 01:44:52 +0700

    already fixed the map

--- src/App.js
+++ src/App.js
@@ -46,67 +46,8 @@ function App() {
       </Modal></>
         <div className='container'>
       
-        <img src={canteen} style={{width:'100%' , height:'100%' , maxWidth:'802px',maxHeight:'844px'}} className="map"/>
-          <div className='marker' style={{top: '30%' , left: '35%'}}   onClick={(e)=>handleclick({name:'blue'})}>
-          <img src={mark} style={{width:'5%',maxWidth:'700px',maxHeight:'700px'}} />
-          </div>
-
-          <div className='marker' style={{top: '40%' , left: '47%'}} onClick={(e)=>handleclick({name:'student_dorm'})}>
-          <img src={mark} style={{width:'5%',maxWidth:'700px',maxHeight:'700px'}} />
-          </div>
-
-          <div className='marker' style={{top: '52%' , left: '47%'}} >
-          <img src={mark} style={{width:'5%',maxWidth:'700px',maxHeight:'700px'}} />
-          </div>
-
-          <div className='marker' style={{top: '52%' , left: '47%'}} >
-          <img src={mark} style={{width:'5%',maxWidth:'700px',maxHeight:'700px'}} />
-          </div>
-
-          <div className='marker' style={{top: '65%' , left: '42%'}} >
-          <img src={mark} style={{width:'5%',maxWidth:'700px',maxHeight:'700px'}} />
-          </div>
-
-          <div className='marker' style={{top: '75%' , left: '50%'}} >
-          <img src={mark} style={{width:'5%',maxWidth:'700px',maxHeight:'700px'}} />
-          </div>
-
-          
-          <div className='marker' style={{top: '75%' , left: '32%'}} >
-          <img src={mark} style={{width:'5%',maxWidth:'700px',maxHeight:'700px'}} />
-          </div>
-
-          <div className='marker' style={{top: '73%' , left: '66%'}} >
-          <img src={mark} style={{width:'5%',maxWidth:'700px',maxHeight:'700px'}} />
-          </div>
-
-          <div className='marker' style={{top: '90%' , left: '73%'}} >
-          <img src={mark} style={{width:'5%',maxWidth:'700px',maxHeight:'700px'}} />
-          </div>
-
-          <div className='marker' style={{top: '85%' , left: '85%'}} >
-          <img src={mark} style={{width:'5%',maxWidth:'700px',maxHeight:'700px'}} />
-          </div>
-
-          <div className='marker' style={{top: '63%' , left: '90%'}} >
-          <img src={mark} style={{width:'5%',maxWidth:'700px',maxHeight:'700px'}} />
-          </div>
-
-          <div className='marker' style={{top: '48%' , left: '90%'}} >
-          <img src={mark} style={{width:'5%',maxWidth:'700px',maxHeight:'700px'}} />
-          </div>
-
-          <div className='marker' style={{top: '45%' , left: '65%'}} >
-          <img src={mark} style={{width:'5%',maxWidth:'700px',maxHeight:'700px'}} />
-          </div>
-
-          <div className='marker' style={{top: '23%' , left: '65%'}} >
-          <img src={mark} style={{width:'5%',maxWidth:'700px',maxHeight:'700px'}} />
-          </div>
-
-          <div className='marker' style={{top: '19%' , left: '80%'}} >
-          <img src={mark} style={{width:'5%',maxWidth:'700px',maxHeight:'700px'}} />
-          </div>
+        <img src={canteen}  className="map"/>
+       
         </div>
         </>
     );
