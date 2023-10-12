2944a0bbed97b2e0db61e1dd58fb5e2555d31d79
ThanawaratGithub <lopamarkin@gmail.com>
2023-10-12 17:48:39 +0700

    update card according to the design

--- src/App.js
+++ src/App.js
@@ -1,108 +1,260 @@
-import canteen from './canteenmap.jpg'
-import mark from './ggmark.jpg'
-import star from './star_icon.jpg'
-import './App.css';
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
-import RecipeReviewCard from './components/Cardcompo';
-import { List, Paper } from '@mui/material';
-import { database } from './DB';
+import canteen from "./canteenmap.jpg";
+import mark from "./ggmark.jpg";
+import star from "./star_icon.jpg";
+import "./App.css";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import RecipeReviewCard from "./components/Cardcompo";
+import { List, Paper } from "@mui/material";
+import { database } from "./DB";
+import ClearIcon from '@mui/icons-material/Clear';
 function App() {
   const [open, setOpen] = React.useState(false);
-  const [showlist,setShow] = React.useState([0]);
-  const [modalname,setModaname] = React.useState('');
+  const [showlist, setShow] = React.useState([0]);
+  const [modalname, setModaname] = React.useState("");
   const handleClose = () => setOpen(false);
-  const handleclick = (event)=>{
-    console.log(event)
-    let listofrec = []
+  const handleclick = (event) => {
+    console.log(event);
+    let listofrec = [];
 
-    if(event['status']){
-      console.log(database[event['val_id']])
-      let compo = database[event['val_id']];
-      setModaname(compo['canteen_name'])
-      for(let i = 0; i<compo['menu'].length ;i++){
-          listofrec.push(<RecipeReviewCard key = {i} props = {compo['menu'][i]}/>)
+    if (event["status"]) {
+      console.log(database[event["val_id"]]);
+      let compo = database[event["val_id"]];
+      setModaname(compo["canteen_name"]);
+      for (let i = 0; i < compo["menu"].length; i++) {
+        listofrec.push(<RecipeReviewCard key={i} props={compo["menu"][i]} />);
       }
-  
 
-      
-      setShow(listofrec)
-      setOpen(true)
-    }else{
-      setModaname('No_receipe')
-      setShow([])
-      setShow(listofrec)
-      setOpen(true)
+      setShow(listofrec);
+      setOpen(true);
+    } else {
+      setModaname("No_receipe");
+      setShow([]);
+      setShow(listofrec);
+      setOpen(true);
     }
-  }
+  };
 
-  
   return (
-        <>
+    <>
+      <>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="mptjomg"
+          aria-describedby="modal-modal-description"
+        >
+          <>
+            <div className="card_container" style={{ padding: "12px" }}>
+              <div >
+                <div style={{ display: "flex", gap: "4px" }}>
+                  <div>
+                    <img src={star}></img>
+                  </div>
+                  <div>
+                    <p>เมนูอาหารสุขภาพแนะนำ</p>
+                  </div>
+                  <ClearIcon sx = {{marginLeft:'auto'}} onClick ={handleClose}/>
+                </div>
+                <p className="canteen_loc"> ณ {modalname} </p>
 
-        <>
-      <Modal
-  
-        open={open}
-        onClose={handleClose}
-        aria-labelledby = 'mptjomg'
-        aria-describedby="modal-modal-description"
-      >
-        <>
-
-        <div className = 'card_container' style={{borderRadius:'8px',paddingBottom:'10px'}}> 
-          <div style={{padding:'10px'}}>
-          <div style={{display:'flex'}}>
-          <div ><img src = {star} ></img></div>
-          <div  ><p>เมนูอาหารสุขภาพ</p></div>
-
-          </div>
-          ณ {modalname}
-          <div style={{margin:'auto',width: '95%', height: '100%', border: '1px #CBD5E1 solid'}}></div>
-          </div>
-          <div>{showlist}</div>
-          
-        
-
-      </div>
-
-        </>
-      </Modal></>
+                <div
+                  style={{
+                    margin: "auto",
+                    width: "100%",
+                    height: "100%",
+                    border: "1px #CBD5E1 solid",
+                  }}
+                ></div>
+              </div>
+              <div>{showlist}</div>
+            </div>
+          </>
+        </Modal>
+      </>
       <div className="container">
-  <div className="map-container">
-    <img src={canteen} className="map" alt="Map" />
-    <img  src = {mark} className="marker" id='sasin' style={{width:'27px',height:'37px',position:'absolute',top:'250px',left:'300px'}} onClick={handleclick}></img>
-    <img  src = {mark} className="marker" id='dorm' style={{width:'27px',height:'37px',position:'absolute',top:'370px',left:'400px'}}></img>
-    <img  src = {mark} className="marker" id='horklang' style={{width:'27px',height:'37px',position:'absolute',top:'450px',left:'420px'}}></img>
-        <img  src = {mark} className="marker" id='cud' style={{width:'27px',height:'37px',position:'absolute',top:'600px',left:'390px'}}></img>
-        <img  src = {mark} className="marker" id='jarmnine' style={{width:'27px',height:'37px',position:'absolute',top:'700px',left:'280px'}}></img>
-        <img  src = {mark} className="commart" id='6' style={{width:'27px',height:'37px',position:'absolute',top:'680px',left:'420px'}} onClick = { ()=> handleclick({'status':true,'val_id':5})}></img>
-        <img  src = {mark} className="marker" id='dent' style={{width:'27px',height:'37px',position:'absolute',top:'170px',left:'730px'}}></img>
-        <img  src = {mark} className="marker" id='pesad' style={{width:'27px',height:'37px',position:'absolute',top:'200px',left:'580px'}}></img>
-        <img  src = {mark} className="marker" id='arch' style={{width:'27px',height:'37px',position:'absolute',top:'400px',left:'565px'}}></img>
-        <img  src = {mark} className="art" id='1' style={{width:'27px',height:'37px',position:'absolute',top:'420px',left:'810px'}} onClick = { ()=> handleclick({'status':true,'val_id':0})}></img>
-        <img  src = {mark} className="marker" id='eng' style={{width:'27px',height:'37px',position:'absolute',top:'555px',left:'800px'}} onClick = { ()=> handleclick({'status':false,'val_id':0})} ></img>
-        <img  src = {mark} className="marker" id='science' style={{width:'27px',height:'37px',position:'absolute',top:'640px',left:'600px'}} onClick = { ()=> handleclick({'status':false,'val_id':0})}></img>
-        <img  src = {mark} className="marker" id='polscience' style={{width:'27px',height:'37px',position:'absolute',top:'740px',left:'780px'}}></img>
-        <img  src = {mark} className="marker" id='econ' style={{width:'27px',height:'37px',position:'absolute',top:'780px',left:'680px'}}></img>
-
-
-
-
-
-
-
-
-
-
-
-  </div>
-</div>
-        </>
-    );
+        <div className="map-container">
+          <img src={canteen} className="map" alt="Map" />
+          <img
+            src={mark}
+            className="marker"
+            id="sasin"
+            style={{
+              width: "27px",
+              height: "37px",
+              position: "absolute",
+              top: "250px",
+              left: "300px",
+            }}
+            onClick={handleclick}
+          ></img>
+          <img
+            src={mark}
+            className="marker"
+            id="dorm"
+            style={{
+              width: "27px",
+              height: "37px",
+              position: "absolute",
+              top: "370px",
+              left: "400px",
+            }}
+          ></img>
+          <img
+            src={mark}
+            className="marker"
+            id="horklang"
+            style={{
+              width: "27px",
+              height: "37px",
+              position: "absolute",
+              top: "450px",
+              left: "420px",
+            }}
+          ></img>
+          <img
+            src={mark}
+            className="marker"
+            id="cud"
+            style={{
+              width: "27px",
+              height: "37px",
+              position: "absolute",
+              top: "600px",
+              left: "390px",
+            }}
+          ></img>
+          <img
+            src={mark}
+            className="marker"
+            id="jarmnine"
+            style={{
+              width: "27px",
+              height: "37px",
+              position: "absolute",
+              top: "700px",
+              left: "280px",
+            }}
+          ></img>
+          <img
+            src={mark}
+            className="commart"
+            id="6"
+            style={{
+              width: "27px",
+              height: "37px",
+              position: "absolute",
+              top: "680px",
+              left: "420px",
+            }}
+            onClick={() => handleclick({ status: true, val_id: 5 })}
+          ></img>
+          <img
+            src={mark}
+            className="marker"
+            id="dent"
+            style={{
+              width: "27px",
+              height: "37px",
+              position: "absolute",
+              top: "170px",
+              left: "730px",
+            }}
+          ></img>
+          <img
+            src={mark}
+            className="marker"
+            id="pesad"
+            style={{
+              width: "27px",
+              height: "37px",
+              position: "absolute",
+              top: "200px",
+              left: "580px",
+            }}
+          ></img>
+          <img
+            src={mark}
+            className="marker"
+            id="arch"
+            style={{
+              width: "27px",
+              height: "37px",
+              position: "absolute",
+              top: "400px",
+              left: "565px",
+            }}
+          ></img>
+          <img
+            src={mark}
+            className="art"
+            id="1"
+            style={{
+              width: "27px",
+              height: "37px",
+              position: "absolute",
+              top: "420px",
+              left: "810px",
+            }}
+            onClick={() => handleclick({ status: true, val_id: 0 })}
+          ></img>
+          <img
+            src={mark}
+            className="marker"
+            id="eng"
+            style={{
+              width: "27px",
+              height: "37px",
+              position: "absolute",
+              top: "555px",
+              left: "800px",
+            }}
+            onClick={() => handleclick({ status: false, val_id: 0 })}
+          ></img>
+          <img
+            src={mark}
+            className="marker"
+            id="science"
+            style={{
+              width: "27px",
+              height: "37px",
+              position: "absolute",
+              top: "640px",
+              left: "600px",
+            }}
+            onClick={() => handleclick({ status: false, val_id: 0 })}
+          ></img>
+          <img
+            src={mark}
+            className="marker"
+            id="polscience"
+            style={{
+              width: "27px",
+              height: "37px",
+              position: "absolute",
+              top: "740px",
+              left: "780px",
+            }}
+          ></img>
+          <img
+            src={mark}
+            className="marker"
+            id="econ"
+            style={{
+              width: "27px",
+              height: "37px",
+              position: "absolute",
+              top: "780px",
+              left: "680px",
+            }}
+          ></img>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/components/Cardcompo.js
+++ src/components/Cardcompo.js
@@ -1,34 +1,33 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import './Cardcompo.css'
-
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import "./Cardcompo.css";
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
   return <IconButton {...other} />;
 })(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
     duration: theme.transitions.duration.shortest,
   }),
 }));
 
 export default function RecipeReviewCard(props) {
-  let db = props.props
+  let db = props.props;
   const [expanded, setExpanded] = React.useState(false);
 
   const handleExpandClick = () => {
@@ -36,45 +35,74 @@ export default function RecipeReviewCard(props) {
   };
 
   return (
-    <Card sx = {{width:'98%',marginLeft:'auto',marginRight:'auto',fontFamily:'Noto Sans Thai, sans-serif'  }}>
-      <div className='box_container' onClick={handleExpandClick}>
-        <div className='img_container'>
-          <img src= 'https://shorturl.at/dkuKU' style={{width:'74px', height:'66px',borderRadius: 4}}></img>
-        </div>
-        <div className='res_detail'>
-          <div className='detail_head'>
-          <p style={{color: '#7FCB9B', fontSize: 12, fontFamily: 'IBM Plex Sans Thai'}}>{db.restaurant_name}</p>
-          <p style={{color: 'black', fontSize: 12, fontFamily: 'IBM Plex Sans Thai'}}>{db.price} บาท </p>
+    <>
+      <Card
+        sx={{
+          width: "100%",
+          marginTop: "12px",
+          fontFamily: "Noto Sans Thai, sans-serif",
+          border: "1px solid var(--base-border-light, #E2E8F0)",
+          borderRadius: "12px",
+          box_shadow: "0px 1px 2px 0px rgba(30, 41, 59, 0.05)",
+        }}
+      >
+        <div className="content_container" onClick={handleExpandClick}>  
+          <div className="img_container">    
+            <img
+              src="https://shorturl.at/dkuKU"
+              style={{ width: "74px", height: "66px", borderRadius: "4px" }}
+            ></img>
           </div>
-          <div className='detail_tail'>
-          <p style={{color: 'black', fontSize: 16, fontFamily: 'IBM Plex Sans Thai'}}>{props.props.menu_name}</p>
-          </div>
-        </div>
-      </div>
-     
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph><p style={{color:'#3B82F6'}}>รายละเอียด</p></Typography>
-         
-         
-          <Typography paragraph>
-            {db.menu_detail}
-          </Typography>
-          <Typography paragraph>
-           <p style={{color:'#3B82F6'}}>รีวิว</p>
-          </Typography>
-          <Typography >
-            <div style={{display:'inline-block'}}>
-            <p>รสชาติ</p> {db.taste_review}
+          <div className="res_detail">
+            <div className="detail_head">
+              <p className="res_name"
+
+              >
+                {db.restaurant_name}
+              </p>
+              <div className="price_baht">
+              <p
+                className="price"
+              >
+                {db.price}
+              </p>
+                <p className="baht"
+              >
+                    บาท
+              </p>
+              </div>
             </div>
-          </Typography>
+            <div className="detail_tail">
+              <p
+                style={{
+                  color: "black",
+                  fontSize: 16,
+                  fontFamily: "IBM Plex Sans Thai",
+                  width:'90%'
+                }}
+              >
+                {props.props.menu_name}
+              </p>
+            </div>
+          </div>
+        </div>
 
-          <Typography>
-            <p>ราคา </p> {db.price_review}
-          </Typography>
-
-        </CardContent>
-      </Collapse>
-    </Card>
+        <Collapse in={expanded} timeout="auto" unmountOnExit className="detail_container" sx = {{width:'95%'}} >
+           <div onClick={handleExpandClick} style = {{margin:'0px 8px 8px 8px'}} className="detail" >
+            รายละเอียด
+           </div>
+           <div onClick={handleExpandClick} style = {{margin:'8px'}} className="review">
+           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {db.menu_detail}
+           </div>
+           <div onClick={handleExpandClick} style = {{margin:'8px'}} className="cus_review">
+            รีวิว
+           </div>
+           <div onClick={handleExpandClick} style = {{margin:'8px'}} className="review_detail">
+           <p></p><strong>รสชาติ:</strong> รสชาติ ไม่จัดเกินไป ไม่เค็มเกิน และไม่ใส่ผงชูรส แต่บางเมนูอาจจะจืดไปหน่อย
+           <p></p><strong>ราคา:</strong>  ราคาน่ารักเหมาะกับนิสิตในงบจำกัด และได้กับข้าวถึง3อย่าง
+           </div>
+        </Collapse>
+      </Card>
+    </>
   );
 }
