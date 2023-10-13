8b4616d9b45c388e07f0455cb63e8a5a43acf053
ThanawaratGithub <lopamarkin@gmail.com>
2023-10-13 14:45:42 +0700

    fixed picture size

--- src/components/Cardcompo.js
+++ src/components/Cardcompo.js
@@ -3,10 +3,9 @@ import Card from "@mui/material/Card";
 import Collapse from "@mui/material/Collapse";
 import "./Cardcompo.css";
 
-
 export default function RecipeReviewCard(props) {
   let db = props.props;
-  console.log(db.image_path)
+  console.log(db.image_path);
   const [expanded, setExpanded] = React.useState(false);
 
   const handleExpandClick = () => {
@@ -25,34 +24,22 @@ export default function RecipeReviewCard(props) {
           box_shadow: "0px 1px 2px 0px rgba(30, 41, 59, 0.05)",
         }}
       >
-        <div className="content_container" onClick={handleExpandClick}>  
-          <div className="img_container" style={{
-            backgroundColor:'red',
-            width:'74px',
-            height:'66px',
-            borderRadius:'4px',
+        <div className="content_container" onClick={handleExpandClick}>
+
+   <div className="img_container" style={{
+            minWidth:'74px',height:'66px',maxHeight:'66px',
+            borderRadius:'8px',
             backgroundSize:'120% 120%',
-            background: 'url'+'('+ process.env.PUBLIC_URL + '/food_image/'+ db.image_path +'.jpg'+")"+ 'lightgray 50% / cover no-repeat'
+            background: 'url'+'('+ process.env.PUBLIC_URL + "/food_image/" + db.image_path + ".jpg"+")"+ 'lightgray 50% / cover no-repeat'
           }}>    
 
           </div>
           <div className="res_detail">
             <div className="detail_head">
-              <p className="res_name"
-
-              >
-                {db.restaurant_name}
-              </p>
+              <p className="res_name">{db.restaurant_name}</p>
               <div className="price_baht">
-              <p
-                className="price"
-              >
-                {db.price}
-              </p>
-                <p className="baht"
-              >
-                    บาท
-              </p>
+                <p className="price">{db.price}</p>
+                <p className="baht">บาท</p>
               </div>
             </div>
             <div className="detail_tail">
@@ -61,7 +48,7 @@ export default function RecipeReviewCard(props) {
                   color: "black",
                   fontSize: 16,
                   fontFamily: "IBM Plex Sans Thai",
-                  width:'90%'
+                  width: "90%",
                 }}
               >
                 {props.props.menu_name}
@@ -70,20 +57,44 @@ export default function RecipeReviewCard(props) {
           </div>
         </div>
 
-        <Collapse in={expanded} timeout="auto" unmountOnExit className="detail_container" sx = {{width:'95%'}} >
-           <div onClick={handleExpandClick} style = {{margin:'0px 8px 8px 8px'}} className="detail" >
+        <Collapse
+          in={expanded}
+          timeout="auto"
+          unmountOnExit
+          className="detail_container"
+          sx={{ width: "95%" }}
+        >
+          <div
+            onClick={handleExpandClick}
+            style={{ margin: "0px 8px 8px 8px" }}
+            className="detail"
+          >
             รายละเอียด
-           </div>
-           <div onClick={handleExpandClick} style = {{margin:'8px'}} className="review">
-           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {db.menu_detail}
-           </div>
-           <div onClick={handleExpandClick} style = {{margin:'8px'}} className="cus_review">
+          </div>
+          <div
+            onClick={handleExpandClick}
+            style={{ margin: "8px" }}
+            className="review"
+          >
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {db.menu_detail}
+          </div>
+          <div
+            onClick={handleExpandClick}
+            style={{ margin: "8px" }}
+            className="cus_review"
+          >
             รีวิว
-           </div>
-           <div onClick={handleExpandClick} style = {{margin:'8px'}} className="review_detail">
-           <p></p><strong>รสชาติ:</strong> รสชาติ ไม่จัดเกินไป ไม่เค็มเกิน และไม่ใส่ผงชูรส แต่บางเมนูอาจจะจืดไปหน่อย
-           <p></p><strong>ราคา:</strong>  ราคาน่ารักเหมาะกับนิสิตในงบจำกัด และได้กับข้าวถึง3อย่าง
-           </div>
+          </div>
+          <div
+            onClick={handleExpandClick}
+            style={{ margin: "8px" }}
+            className="review_detail"
+          >
+            <p></p>
+            <strong>รสชาติ:</strong> {db.taste_review}
+            <p></p>
+            <strong>ราคา:</strong> {db.price_review}
+          </div>
         </Collapse>
       </Card>
     </>
